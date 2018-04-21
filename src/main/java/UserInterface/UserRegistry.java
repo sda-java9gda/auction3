a94ac10c471801915487f84527525dd7a4e894ea
Yurii Halatiuk/Yurec <yuriy.galatyuk@gmail.com>
2018-04-21 11:12:57 +0200

    new coommit userRegistry

--- src/main/java/UserInterface/UserRegistry.java
+++ src/main/java/UserInterface/UserRegistry.java
@@ -1,5 +1,12 @@
 package UserInterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRegistry {
+    private UserData login;
+
+private List<UserData> users = new ArrayList<>();
+
 
 }
