039eac8d5f766940e5d6c04e70dfb5f78634e151
Jarek Kijko/kijek1979 <jwkjk@o2.pl>
2018-04-21 10:37:47 +0200

    Commit UserInterface

---
+++ src/main/java/UserInterface/UserData.java
@@ -0,0 +1,13 @@
+package UserInterface;
+
+public class UserData {
+
+    private String loginName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+
+
+
+}
